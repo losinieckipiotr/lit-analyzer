aa1d9b3ba469408bce585342134f6f90034a60d5
losinieckipiotr <losinieckipiotr@gmail.com>
2026-08-23 06:52:46 +0200

    updated parse5 to v6

--- packages/lit-analyzer/src/lib/analyze/parse/document/text-document/html-document/parse-html-p5/parse-html.ts
+++ packages/lit-analyzer/src/lib/analyze/parse/document/text-document/html-document/parse-html-p5/parse-html.ts
@@ -1,7 +1,7 @@
 import { IP5CommentNode, IP5DocumentFragmentNode, IP5NodeBase, IP5TagNode, IP5TextNode, P5Node } from "./parse-html-types.js";
 
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { parseFragment } = require("parse5");
+// @ts-expect-error no types for now
+import { parseFragment } from "parse5";
 
 /**
  * Returns if a p5Node is a tag node.
