811a40076f7fb90e73e35f018b0c089f1fec4065
losinieckipiotr <losinieckipiotr@gmail.com>
2026-08-26 19:37:01 +0200

    lit-analyzer v3.0.0

--- packages/lit-analyzer/src/lib/analyze/constants.ts
+++ packages/lit-analyzer/src/lib/analyze/constants.ts
@@ -1,10 +1,10 @@
 export type LitHtmlAttributeModifier = "." | "?" | "@";
 
-export const LIT_HTML_PROP_ATTRIBUTE_MODIFIER = ".";
+export const DIAGNOSTIC_SOURCE = "lit-plugin";
 
 export const LIT_HTML_BOOLEAN_ATTRIBUTE_MODIFIER = "?";
-
 export const LIT_HTML_EVENT_LISTENER_ATTRIBUTE_MODIFIER = "@";
+export const LIT_HTML_PROP_ATTRIBUTE_MODIFIER = ".";
 
 export const LIT_HTML_ATTRIBUTE_MODIFIERS: LitHtmlAttributeModifier[] = [
   LIT_HTML_PROP_ATTRIBUTE_MODIFIER,
@@ -12,10 +12,8 @@ export const LIT_HTML_ATTRIBUTE_MODIFIERS: LitHtmlAttributeModifier[] = [
   LIT_HTML_EVENT_LISTENER_ATTRIBUTE_MODIFIER,
 ];
 
-export const DIAGNOSTIC_SOURCE = "lit-plugin";
+export const MAX_RUNNING_TIME_PER_OPERATION = 150; // Default to small timeouts. Opt in to larger timeouts where necessary.
 
 export const TS_IGNORE_FLAG = "@ts-ignore";
 
 export const VERSION = "2.0.3";
-
-export const MAX_RUNNING_TIME_PER_OPERATION = 150; // Default to small timeouts. Opt in to larger timeouts where necessary.
