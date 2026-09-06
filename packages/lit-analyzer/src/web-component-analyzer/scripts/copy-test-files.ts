1bb6222a7297ad3a832158f1e395dfd34f6a5a8c
losinieckipiotr <losinieckipiotr@gmail.com>
2026-09-06 16:55:29 +0200

    cleanup

--- packages/lit-analyzer/src/web-component-analyzer/scripts/copy-test-files.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-console */
-import fs from "fs";
-
-console.log("Copying 'test/flavors/lwc' to 'dist/test/flavors/lwc'...");
-
-fs.cpSync("test/flavors/lwc/comp", "dist/test/flavors/lwc/comp", {
-  recursive: true
-});
--- packages/lit-analyzer/src/web-component-analyzer/src/cli/util/file-util.ts
+++
@@ -1,9 +0,0 @@
-import { mkdirSync } from "fs";
-
-export function ensureDirSync(dir: string): void {
-  try {
-    mkdirSync(dir, { recursive: true });
-  } catch (err) {
-    if ((err as Error & { code: string }).code !== "EEXIST") throw err;
-  }
-}
